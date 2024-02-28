39e26fca42bad5ce650573de7549cc5b088a4c13
Jackson Massey <jackmassey2000@gmail.com>
2024-02-28 14:44:36 -0600

    modified description

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # IMGGAMING-API
 # NOTE: This code library is not intented for. nor capable of performing, illegal activites/ripping. It can only be used by authenticated ufcfightpass.com subscription holders.
+# This library cannot download video content.
 
 This API client currently supports the UFC Fight Pass portion of the API, browsing the fightpass library, performing searches, exploring playlists, etc. 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='imggaming',
     version='0.1',
     packages=find_packages(),
-    description='A python wrapper for the imggaming api.',
+    description='A REST api client for the imggaming api, written in python.',
     author='jackinthebox52',
     author_email='jackmassey2000@gmail.com',
     url='https://github.com/jackinthebox52/imggaming',
@@ -13,7 +13,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    python_requires='>=3.6, <4',
     install_requires=[
         'requests',
     ],
+    keywords='imggaming, api, wrapper, client',
 )
